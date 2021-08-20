fef3a7c3d290adf6f57efccb6f4fabcb116e19dc
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2021-08-20 19:40:58 +0300

    Initial commit

---
+++ src/Vertex.java
@@ -0,0 +1,23 @@
+import java.util.Objects;
+
+class Vertex {
+    String label;
+    Vertex(String label) {
+        this.label = label;
+    }
+
+    // equals and hashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return label.equals(vertex.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
+    }
+}
